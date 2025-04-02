bc38716b21ca7fca3238442e1ebd0a65d7bc0c5b
Francisco Reyna <francisco.reyna@grupolufesa.com>
2025-04-01 21:54:07 -0600

    Feat: Adding environment files Feat: Adding currency pipe Feat: change the API:URL TO API_URL ENV file

--- src/app/services/reportefinanciero.service.ts
+++ src/app/services/reportefinanciero.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Reporte } from '../models/reporteFinanciero.model';
+import { environment } from '../../environments/environment.staging';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class ReporteFinancieroService {
 
   getData(year: number, month: string, udn = "default"): Observable<Reporte>
   {
-    let url = `https://erp-api-ps4c.onrender.com/get_reporte_info/${year}/${month}/${udn}`;    
+    let url = `${environment.apiUrl}/get_reporte_info/${year}/${month}/${udn}`;    
     
     return this.httClient.get<Reporte>(url);
   }
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://192.168.50.16:8000',
+    // otras variables...
+  };
